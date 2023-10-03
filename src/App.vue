c36495664fb28ab89ba47aa6304b0346e19dfeb6
Amber Mullen <121705867+armullen@users.noreply.github.com>
2023-10-03 16:52:20 -0500

    end of the day commit

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,8 @@ import { RouterLink, RouterView } from 'vue-router'
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink> | 
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/about">About</RouterLink> | 
+        <RouterLink to="/alldogs">Dogs</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/All-dogs.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="all-dogs">
+      <h1>This is the all dogs page</h1>
+    </div>
+  </template>
+  
+  <style>
+  
+  </style>
